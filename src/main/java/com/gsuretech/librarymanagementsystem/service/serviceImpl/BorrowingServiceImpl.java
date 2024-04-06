41c3bb95440e68979316cf502eaf23f61a8682d4
Abdulganiyu Abubakar <abdulganiyua@hardcorebiometric.com>
2024-04-06 11:45:40 +0100

    implemented the controller for the book

---
+++ src/main/java/com/gsuretech/librarymanagementsystem/service/serviceImpl/BorrowingServiceImpl.java
@@ -0,0 +1,6 @@
+package com.gsuretech.librarymanagementsystem.service.serviceImpl;
+
+import com.gsuretech.librarymanagementsystem.service.BorrowingService;
+
+public class BorrowingServiceImpl implements BorrowingService {
+}
